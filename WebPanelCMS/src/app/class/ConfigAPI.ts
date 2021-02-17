9577ed06801704615a007dfcfc3ae13561ec3c97
Talwinder Jeet Singh <talwindergur@gmail.com>
2021-02-17 11:44:10 +0530

    playlist issue

--- WebPanelCMS/src/app/class/ConfigAPI.ts
+++ WebPanelCMS/src/app/class/ConfigAPI.ts
@@ -1,6 +1,6 @@
 export class ConfigAPI {
-  private Host: string = "https://api.advikon.com/api/";
-
+  //private Host: string = "https://api.advikon.com/api/";
+  private Host: string = "http://localhost:60328/api/";
    FillQueryCombo: string = this.Host + 'FillQueryCombo';
    FillTokenInfo: string = this.Host + 'FillTokenInfo';
    FillTokenContent: string = this.Host + 'FillTokenContent';
